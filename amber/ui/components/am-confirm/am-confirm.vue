59c10e75be04276d3cf90e4a01e3fe8198744c7d
btqf <2452501500@qq.com>
2023-01-29 17:19:36 +0800

    feat: add user-pages

--- amber/ui/components/am-confirm/am-confirm.vue
+++ amber/ui/components/am-confirm/am-confirm.vue
@@ -1,1 +1,7 @@
+<template>
+  <view class="am-confirm"> </view>
+</template>
 
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- amber/ui/components/am-sticky/am-sticky.vue
+++ amber/ui/components/am-sticky/am-sticky.vue
@@ -1,11 +1,11 @@
 <template>
-	<view
-		class="am-sticky"
-		:class="[{ 'is-flex': props.isFlex }]"
-		:style="{ zIndex, top: stickyTop }"
-	>
-		<slot></slot>
-	</view>
+  <view
+    class="am-sticky"
+    :class="[{ 'is-flex': props.isFlex }]"
+    :style="{ zIndex, top: stickyTop }"
+  >
+    <slot></slot>
+  </view>
 </template>
 
 <script setup lang="ts">
@@ -21,15 +21,15 @@ import { computed } from "vue";
 import { useAmber } from "@/amber";
 
 type Props = {
-	zIndex?: number;
-	top?: number | string;
-	isFlex?: boolean;
+  zIndex?: number;
+  top?: number | string;
+  isFlex?: boolean;
 };
 
 const props = withDefaults(defineProps<Props>(), {
-	zIndex: 999,
-	top: 0,
-	isFlex: false,
+  zIndex: 999,
+  top: 0,
+  isFlex: false,
 });
 
 const { statusBarHeight } = uni.getSystemInfoSync();
@@ -37,23 +37,23 @@ const { statusBarHeight } = uni.getSystemInfoSync();
 const { router } = useAmber();
 
 const stickyTop = computed(() => {
-	// 状态栏高度
-	const top1 = `${statusBarHeight}px`;
+  // 状态栏高度
+  const top1 = `${statusBarHeight}px`;
 
-	// 标题栏高度
-	// #ifdef H5
-	const top2 = "44px";
-	// #endif
-	// #ifndef H5
-	const top2 = "0px";
-	// #endif
+  // 标题栏高度
+  // #ifdef H5
+  const top2 = "44px";
+  // #endif
+  // #ifndef H5
+  const top2 = "0px";
+  // #endif
 
-	// 自定义高度
-	const top = isNumber(props.top) ? props.top + "rpx" : props.top;
+  // 自定义高度
+  const top = isNumber(props.top) ? props.top + "rpx" : props.top;
 
-	// 计算后距离顶部高度
-	const arr = [top, ...(router.info()?.isCustomNavbar ? [top1] : [top2])];
-	return `calc(${arr.join(" + ")})`;
+  // 计算后距离顶部高度
+  const arr = [top, ...(router.info()?.isCustomNavbar ? [top1] : [top2])];
+  return `calc(${arr.join(" + ")})`;
 });
 </script>
 
